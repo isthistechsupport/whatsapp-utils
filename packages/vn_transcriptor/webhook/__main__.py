3b5ef9925971d7805da93974a74772d4f529d53c
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-04-18 02:24:04 -0500

    added file type, file size and file hash safeguards

--- packages/vn_transcriptor/webhook/__main__.py
+++ packages/vn_transcriptor/webhook/__main__.py
@@ -42,7 +42,7 @@ def validate_mime_type(audio_mime_type: str) -> bool:
     return any(mime_type in audio_mime_type for mime_type in valid_mime_types)
 
 
-def get_audio_file(audio_id: str) -> BytesIO:
+def get_audio_file(audio_id: str, phone_number_id: str, sender: str) -> BytesIO:
     url = f"https://graph.facebook.com/v19.0/{audio_id}/"
     headers = {
         'Authorization': f'Bearer {os.environ.get("GRAPH_API_TOKEN")}'
@@ -50,23 +50,36 @@ def get_audio_file(audio_id: str) -> BytesIO:
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     response_json = response.json()
-    file_url, file_hash = response_json()['url'], response_json()['sha256']
+    file_url, file_hash, file_mime_type, file_size = response_json['url'], response_json['sha256'], response_json['mime_type'], response_json['file_size']
+    if not validate_mime_type(file_mime_type):
+        process_text(
+            phone_number_id,
+            sender,
+            f"Lo siento, el formato del audio no es válido. Los formatos válidos son: flac, mp3, mp4, mpeg, mpga, m4a, ogg, wav y webm. El formato del audio que enviaste es: ```{file_mime_type}```"
+        )
+        raise requests.HTTPError()
+    if file_size > 25 * 1024 * 1024:
+        process_text(
+            phone_number_id,
+            sender,
+            f"Lo siento, el tamaño del audio es muy grande. El tamaño máximo permitido es de 25MB. El tamaño del audio que enviaste es: ```{file_size} bytes, {file_size / (1024 * 1024)} MB```"
+        )
+        raise requests.HTTPError()
     file_response = requests.get(file_url, headers=headers)
     file_response.raise_for_status()
     hashed_content = hashlib.sha256(file_response.content).hexdigest()
     if hashed_content != file_hash:
+        process_text(
+            phone_number_id,
+            sender,
+            f"Lo siento, el audio que enviaste está corrupto. Por favor, intenta enviarlo de nuevo. ```{hashed_content} != {file_hash}```"
+        )
         raise requests.HTTPError()
     return BytesIO(file_response.content)
 
 
-def process_audio(audio_id: str, audio_mime_type: str, phone_number_id: str, sender: str, client: OpenAI):
-    if not validate_mime_type(audio_mime_type):
-        process_text(
-            phone_number_id,
-            sender,
-            f"Lo siento, el formato del audio no es válido. Los formatos válidos son: flac, mp3, mp4, mpeg, mpga, m4a, ogg, wav y webm. El formato del audio que enviaste es: ```{audio_mime_type}```"
-        )
-    with get_audio_file(audio_id) as audio_file:
+def process_audio(audio_id: str, phone_number_id: str, sender: str, client: OpenAI):
+    with get_audio_file(audio_id, phone_number_id, sender) as audio_file:
         transcription = client.audio.transcriptions.create(
             model="whisper-1", 
             file=audio_file,
@@ -91,7 +104,6 @@ def process_change(change: dict, client: OpenAI):
         if message['type'] == 'audio':
             process_audio(
                 audio_id=message['audio']['id'],
-                audio_mime_type=message['audio']['mime_type'],
                 phone_number_id=metadata['phone_number_id'],
                 sender=message['from'],
                 client=client
