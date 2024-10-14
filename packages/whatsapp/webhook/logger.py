3e40a39287629051ad48412134bbec3ce4405f6a
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-10-14 12:23:25 -0500

    added more logging settings

---
+++ packages/whatsapp/webhook/logger.py
@@ -0,0 +1,67 @@
+import sys
+import socket
+import logging
+import logging.config
+
+
+class ContextFilter(logging.Filter):
+    """This filter adds the hostname and name to the log message"""
+    hostname: str = socket.gethostname()
+    def filter(self, record):
+        record.hostname = ContextFilter.hostname
+        return True
+
+
+class LevelFilter(logging.Filter):
+    """This filter directs log records based on their level (used for stdout and stderr)"""
+    def __init__(self, level: int):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno <= self.level
+
+    
+def init_logging():
+    """Initializes the logging system. This should be called at the beginning of the program."""
+    CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s %(hostname)s %(levelname)s tootbot: %(message)s",
+                "datefmt": "%b %d %H:%M:%S",
+            },
+        },
+        "filters": {
+            "level_filter": {
+                "()": LevelFilter,
+                "level": logging.WARNING,
+            },
+            "context_filter": {
+                "()": ContextFilter,
+            },
+        },
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "stream": sys.stdout,
+                "formatter": "default",
+                "filters": ["context_filter", "level_filter"],
+                "level": logging.INFO
+            },
+            "stderr": {
+                "class": "logging.StreamHandler",
+                "stream": sys.stderr,
+                "formatter": "default",
+                "filters": ["context_filter"],
+                "level": logging.ERROR,
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["stdout"],
+                "level": logging.INFO,
+            },
+        },
+    }
+    logging.config.dictConfig(CONFIG)
